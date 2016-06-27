cbcdd32b78f544471572429e8d2a31fef146b8a4
ejikMd <ejik@mail.md>
2016-06-27 10:29:32 -0400

    correct default page

--- R3/Controllers/HomeController.cs
+++ R3/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace R3.Controllers
 
             if (userIpAddress == "::1" || userIpAddress == "173.231.116.114" || userIpAddress == "69.165.203.106")
             {
-                return RedirectToAction("Map","Home");
+                return RedirectToAction("MainListing","Home");
             }
             
             ViewBag.Message = "Parser";
